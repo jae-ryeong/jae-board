ac64dc4cdc63a3e0e4092454ec343868853fa2c0
김재령 <wofud0321@naver.com>
2023-03-09 14:17:43 +0900

    first commit

---
+++ src/main/java/com/example/jaeboard/JaeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.jaeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaeBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jaeboard/JaeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
